693116b389a6854999034335c17184ed7cce6650
Abderrazak Naceur <therzeos@gmail.com>
2019-01-23 22:40:32 +0100

    Add files via upload

---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb://Zak87:Px5XTGh3cuipwSd@tmcluster-shard-00-00-skzqz.mongodb.net:27017,tmcluster-shard-00-01-skzqz.mongodb.net:27017,tmcluster-shard-00-02-skzqz.mongodb.net:27017/test?ssl=true&replicaSet=TMCluster-shard-0&authSource=admin&retryWrites=true',
+    secretOrKey: 'SECRET'
+  };
